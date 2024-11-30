c2f0d7d056b6961a7cf315e7784b3f0bb9da7b76
Richard Bramley <rabramley@gmail.com>
2024-11-30 13:27:24 +0000

    Push REDCap dockers

--- build.sh
+++ build.sh
@@ -9,11 +9,10 @@
 # docker build --no-cache -t lcbruit/identity_web:latest ./identity/web
 # docker build --no-cache -t lcbruit/identity_celery:latest ./identity/celery
 
-# # REDCap
-# docker build -t lcbruit/redcapbase:latest ./redcapbase
+# REDCap
+docker build -t lcbruit/redcapbase:latest ./redcapbase
 
 ## REDCap Specific
-docker build -t lcbruit/redcapbase:latest ./redcapbase
 docker build --build-arg redcap_directory=redcap_dev --build-arg image_type=dev -t lcbruit/redcap_dev:latest ./redcap_specific
 docker build --build-arg redcap_directory=genvasc_redcap --build-arg image_type=genvasc -t lcbruit/redcap_genvasc:latest ./redcap_specific
 docker build --build-arg redcap_directory=redcap --build-arg image_type=uhl -t lcbruit/redcap_uhl:latest ./redcap_specific
--- push.sh
+++ push.sh
@@ -11,3 +11,11 @@ docker push lcbruit/identity_celery:latest
 
 # REDCap
 docker push lcbruit/redcapbase:latest
+
+## REDCap Specific
+docker push lcbruit/redcap_dev:latest
+docker push lcbruit/redcap_genvasc:latest
+docker push lcbruit/redcap_uhl:latest
+docker push lcbruit/redcap_n3:latest
+docker push lcbruit/redcap_national:latest
+docker push lcbruit/redcap_test:latest
