dcca3894d421b4de46794bf1fb7720e9537a3d1f
Richard Bramley <rabramley@gmail.com>
2024-11-30 13:24:41 +0000

    REDCap docker files

--- push.sh
+++ push.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Python
 docker push lcbruit/linuxbase:latest
 docker push lcbruit/pythonbase:latest
 docker push lcbruit/pythonwebappbase:latest
+
+## Identity
 docker push lcbruit/identity_web:latest
 docker push lcbruit/identity_celery:latest
+
+# REDCap
+docker push lcbruit/redcapbase:latest
